7a1b8819614d0503c0850f158d2a641ce9b39d85
moa3337 <moa-x@hotmail.com>
2023-02-27 15:07:52 +0100

    setting cod

---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<template>
+    <div>
+        <a href="#">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </a>
+        <ul>
+            <li><a href="">CHARACTERS</a></li>
+            <li><a href="">COMICS</a></li>
+            <li><a href="">MOVIESS</a></li>
+            <li><a href="">TV</a></li>
+            <li><a href="">GAMES</a></li>
+            <li><a href="">COLLECTIBLES</a></li>
+            <li><a href="">VIDEOS</a></li>
+            <li><a href="">FANS</a></li>
+            <li><a href="">NEWS</a></li>
+            <li><a href="">SHOP</a></li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss"></style>
