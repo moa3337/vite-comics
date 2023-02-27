e2e8167a5240149566472cb0007892729d1e94d5
moa3337 <moa-x@hotmail.com>
2023-02-27 19:47:46 +0100

    completamento layout

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -2,23 +2,33 @@
 </script>
 
 <template>
-    <div>
+    <div class="nav">
         <a href="#">
-            <img src="../assets/img/dc-logo.png" alt="">
+            <img class="logo" src="../assets/img/dc-logo.png" alt="">
         </a>
-        <ul>
-            <li><a href="">CHARACTERS</a></li>
-            <li><a href="">COMICS</a></li>
-            <li><a href="">MOVIESS</a></li>
-            <li><a href="">TV</a></li>
-            <li><a href="">GAMES</a></li>
-            <li><a href="">COLLECTIBLES</a></li>
-            <li><a href="">VIDEOS</a></li>
-            <li><a href="">FANS</a></li>
-            <li><a href="">NEWS</a></li>
-            <li><a href="">SHOP</a></li>
+        <ul class="nav-list">
+            <li class="nav-lis"><a href="">CHARACTERS</a></li>
+            <li class="nav-lis"><a href="">COMICS</a></li>
+            <li class="nav-lis"><a href="">MOVIESS</a></li>
+            <li class="nav-lis"><a href="">TV</a></li>
+            <li class="nav-lis"><a href="">GAMES</a></li>
+            <li class="nav-lis"><a href="">COLLECTIBLES</a></li>
+            <li class="nav-lis"><a href="">VIDEOS</a></li>
+            <li class="nav-lis"><a href="">FANS</a></li>
+            <li class="nav-lis"><a href="">NEWS</a></li>
+            <li class="nav-lis"><a href="">SHOP</a></li>
         </ul>
+
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.logo {
+    width: 60%;
+}
+
+.nav>ul>li>a:hover {
+    color: #0282F9;
+    box-shadow: 0 2px 0 #0282F9;
+}
+</style>
